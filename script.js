be34e1a69c1f0f5c32c3e7debf295203d8fabbb5
Will Marques <87399122+wxllxvn@users.noreply.github.com>
2022-02-15 22:49:40 -0300

    update 15.02

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const player1 = "X";
 const player2 = "O";
+
+
+
+function refreshVarEspacos(){
+    var a1 = document.getElementById('a1')
+    var a2 = document.getElementById('a2')
+    var a3 = document.getElementById('a3')
+    var b1 = document.getElementById('b1')
+    var b2 = document.getElementById('b2')
+    var b3 = document.getElementById('b3')
+    var c1 = document.getElementById('c1')
+    var c2 = document.getElementById('c2')
+    var c3 = document.getElementById('c3')
+}
+
+
+
 var playtime = player1;
 this.gameover = false;
 
-function refreshTabuleiro(){
-    if (this.gameover){
-        return;
+
+function verificarEspacos(){
+    var testEspaco = document.getElementsByClassName("espaco")
+    for (var i = 0; i < testEspaco.length; i++){
+        if(testEspaco[i].dataset.jogada != ""){
+            var lgc = true
+        }else{
+            return false
+        }
     }
+    return lgc
+}
+
+function refreshTabuleiro(){
+
 
     if (playtime == player1){
         var player = document.getElementById("jodador-da-vez");
@@ -19,12 +47,25 @@ function refreshTabuleiro(){
 }
 
 
+function logicaJogo(){
+     if(a1.getAttribute('data-jogada') === b1.getAttribute('data-jogada') && b1.getAttribute('data-jogada') === c1.getAttribute('data-jogada') && a1.getAttribute('data-jogada'), b1.getAttribute('data-jogada'), c1.getAttribute('data-jogada') != ''){
+         console.log(playtime)
+         return
+     }
+     if(a2.getAttribute('data-jogada') == b2.getAttribute('data-jogada') && b2.getAttribute('data-jogada') == c2.getAttribute('data-jogada') && a2.getAttribute('data-jogada') == c2.getAttribute('data-jogada') && a2.getAttribute('data-jogada'), b2.getAttribute('data-jogada'), c2.getAttribute('data-jogada') != ''){
+        console.log(playtime)
+        return
+     }
+}
+
 function iniciarEspaco(){
     var espacos = document.getElementsByClassName("espaco") 
     console.log(espacos)
     for(var i = 0; i < espacos.length; i++){
         espacos[i].addEventListener('click', function(){
-            if (gameover){
+            if (verificarEspacos()){
+
+                alert('acabou o jogo')
                 return;
             }
 
@@ -33,23 +74,35 @@ function iniciarEspaco(){
                 if (playtime == player1){
 
                     this.innerHTML="<img src='imagens/x.png' class='img-jogada'>"
-                    this.setAttribute("jogada", player1)
+                    //this.setAttribute("jogada", player1)
+                    this.dataset.jogada = player1
                     playtime = player2
                 }else{
 
                     this.innerHTML="<img src='imagens/Círculo.png' class='img-jogada'>"
-                    this.setAttribute("jogada", player2)
+                    //this.setAttribute("jogada", player2)
+                    this.dataset.jogada = player2
                     playtime = player1
 
                 }
 
 
             }
+
             refreshTabuleiro()
+            refreshVarEspacos()
+            logicaJogo()
+
         })
 
     }
 }
 
-refreshTabuleiro()
+
+
+
+
+
+
+
 iniciarEspaco()
